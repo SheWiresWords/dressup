f0e18b8ae038e604e7a1a99397c476cae76aa52c
Alexa Curtis <142436963+alexaycurtis@users.noreply.github.com>
2025-08-17 10:49:34 -0500

    set up javascript

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+class DressUpGame {
+  constructor() {
+    this.categories = {
+      bot: { current: 1, max: 9 },
+      shirt: { current: 1, max: 6 },
+      hair: { current: 1, max: 15 },
+      acc: { current: 0, max: 5 },
+      glasses: { current: 0, max: 4 }
+    };
+    
+    this.init();
+  }
+  
+  init() {
+    this.setupEventListeners();
+    this.updateAllCounters();
+    this.initializeDefaultLayers();
+  }
 
+
+  
+}
